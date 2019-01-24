108dcdcbe4f4013a65704bfdac3fb8aced940547
s1651374 <s1651374@sms.ed.ac.uk>
2019-01-24 16:16:30 +0000

    Added more activities

--- app/src/main/java/com/example/s1651374/papadocapp/PatientInfoActivity.java
+++ app/src/main/java/com/example/s1651374/papadocapp/PatientInfoActivity.java
@@ -1,7 +1,9 @@
 package com.example.s1651374.papadocapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PatientInfoActivity extends AppCompatActivity {
 
@@ -10,4 +12,13 @@ public class PatientInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_info);
     }
+
+    // This function must be updated to connect and send the information to the PapaDoc robot.
+    // The moving to MainMenu could be changed to move to a confirmation/error screen to inform the
+    // receptionist as to whether or not the PapaDoc received the information correctly.
+    public void sendToPapaDoc(View view) {
+        Intent intent = new Intent(PatientInfoActivity.this, ReceptionistMenuActivity.class);
+        PatientInfoActivity.this.startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/s1651374/papadocapp/ReceptionistMenuActivity.java
@@ -0,0 +1,31 @@
+package com.example.s1651374.papadocapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ReceptionistMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receptionist_menu);
+    }
+
+    public void goToScheduleActivity(View view) {
+        /*Intent intent = new Intent(ReceptionistMenuActivity.this, ScheduleActivity.class);
+        ReceptionistMenuActivity.this.startActivity(intent);*/
+    }
+
+    public void goToAppointmentActivity(View view) {
+        /*Intent intent = new Intent(ReceptionistMenuActivity.this, AppointmentActivity.class);
+        ReceptionistMenuActivity.this.startActivity(intent);*/
+    }
+
+    public void goToSignInActivity(View view) {
+        /*Intent intent = new Intent(ReceptionistMenuActivity.this, SignInActivity.class);
+        ReceptionistMenuActivity.this.startActivity(intent);*/
+    }
+
+}
