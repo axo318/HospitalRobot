27b425eb471536ccaffd1ab9cc37c414e6e239da
Likhitha <m.likhi@yahoo.com>
2019-01-31 11:44:26 +0000

    Create the Doctor List of Appointments activity

---
+++ app/src/main/java/com/example/s1651374/papadocapp/DoctorAppListActivity.java
@@ -0,0 +1,39 @@
+package com.example.s1651374.papadocapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+
+public class DoctorAppListActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receptionist_app_list);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapterDoc(new String[]{"John Smith", "Donna Paulsen", "Keira Marsh", "Chi Wong"}, new String[]{"20/07/1968", "17/04/1990", "06/11/2001", "08/09/1982"});
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+}
---
+++ app/src/main/java/com/example/s1651374/papadocapp/MyAdapterDoc.java
@@ -0,0 +1,74 @@
+package com.example.s1651374.papadocapp;
+
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class MyAdapterDoc extends RecyclerView.Adapter<MyAdapterDoc.MyViewHolder> {
+
+    private String[] mDataset;
+    private String[] mDataset2;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        private TextView currency;
+        private TextView value;
+        private TextView colorCode;
+
+        public MyViewHolder(final View itemView) {
+            super(itemView);
+            colorCode = (TextView) itemView.findViewById(R.id.colorCode);
+            currency = (TextView) itemView.findViewById(R.id.currency);
+            value = (TextView) itemView.findViewById(R.id.value);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapterDoc(String[] myDataset, String[] myDataset2) {
+        mDataset = myDataset;
+        mDataset2 = myDataset2;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapterDoc.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.rv_item_doc, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapterDoc.MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.currency.setText(mDataset[position]);
+        holder.value.setText(mDataset2[position]);
+        if (mDataset2[position].equals("20/07/1968")){
+            holder.colorCode.setBackgroundColor(Color.parseColor("#ff6961"));
+        }
+        else if (mDataset2[position].equals("08/09/1982")) {
+            holder.colorCode.setBackgroundColor(Color.parseColor("#88dd77"));
+        } else {
+            holder.colorCode.setBackgroundColor(Color.parseColor("#fdf496"));
+        }
+
+    }
+
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
