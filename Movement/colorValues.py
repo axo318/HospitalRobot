9265dc6ae1d106b1619f01f167be09358610592e
kvasnickaerik <kvasnickaerik@gmail.com>
2019-01-31 15:57:13 +0000

    Add files via upload
    First simple line following for the first demo.
    
    4 motors
    3 color sensors

---
+++ Movement/colorValues.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# Core imports
+import time
+import ev3dev.ev3 as ev3
+
+# Local Imports
+import tutorial as tutorial
+
+print ('Welcome to ev3')
+
+while True:
+    print(tutorial.colorValues())
+
+#tutorial.turnAllMotors()
+
+print ("wait 10sec, then end")
+time.sleep(10)
---
+++ Movement/main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# Core imports
+import time
+import ev3dev.ev3 as ev3
+
+# Local Imports
+import tutorial as tutorial
+
+print ('Welcome to ev3')
+
+tutorial.movement()
+#tutorial.turnAllMotors()
+
+print ("wait 10sec, then end")
+time.sleep(10)
