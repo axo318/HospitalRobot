ddd45f87326b1666b0f14eb1642b6bf7adf9212b
tonyfduggan <tonyfduggan@gmail.com>
2019-02-03 21:39:06 +0000

    Robot Speech Program
    Just need to be able to install relevant software on PI and then this should work.

--- AskQuestion.py
+++ AskQuestion.py
@@ -5,15 +5,21 @@
 #2019-02-01 TonyD: Created File
 
 import csv
+import pyttsx3
+#engine = pyttsx3.init()
+
 
 #TonyD: Imports the questions from a csv file and then calls the ynQuesntion method
 #TODO: Adapt to match database output
 def loadQuestions():
+    answers = []
+
     with open("QuestionsTest.csv",newline ='') as csvfile:
         questionReader = csv.reader(csvfile,delimiter=',',quotechar='|')
         for row in questionReader:
             for question in row:
-                ynQuesntion(question)
+                answers.append(ynQuesntion(question))
+    return answers
 
 #Tony: Used to ask the users questions currently just in terminal
 #TODO: add UI and Return to
@@ -23,15 +29,20 @@ def ynQuesntion(q):
     answer.upper()
     if answer == "YES" or answer == "NO" :
         print("ACCEPTED RESPONSE")
+        return answer
     else:
         print("INVALID RESPONSE")
         ynQuesntion(q)
 
 #TonyD: This will use the voice engine to speak the questtion to the user
-#TODO: Create 
-#def speakQuestion(q):
+#TODO: Create
+def speakQuestion(q):
+    engine.say(q)
+    engine.runAndWait();
+
 
 #TonyD: This is the MAIN secion of the progam all calls through here
 qtext = "does this work?"
-loadQuestions()
-ynQuesntion(qtext)
+answers = loadQuestions()
+print("Responses were as follows")
+print(answers)
