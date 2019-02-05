c8b9f1ce7f59ef3e707f2de4fd62f08d4dec78b1
tonyfduggan <tonyfduggan@gmail.com>
2019-02-05 23:15:24 +0000

    Working TTS Code

--- AskQuestion.py
+++ AskQuestion.py
@@ -3,46 +3,52 @@
 #Designed to be an open template that can be expanded on later with UI elements
 #----CHANGES----
 #2019-02-01 TonyD: Created File
+#2019-02-05 TonyD: Added TTS and commentend all changes
 
 import csv
-import pyttsx3
-#engine = pyttsx3.init()
+import pyttsx
 
 
 #TonyD: Imports the questions from a csv file and then calls the ynQuesntion method
 #TODO: Adapt to match database output
 def loadQuestions():
     answers = []
+    engine = pyttsx.init()
 
-    with open("QuestionsTest.csv",newline ='') as csvfile:
+    with open("QuestionsTest.csv") as csvfile:
         questionReader = csv.reader(csvfile,delimiter=',',quotechar='|')
         for row in questionReader:
             for question in row:
-                answers.append(ynQuesntion(question))
+                answers.append(ynQuesntion(question,engine))
     return answers
 
 #Tony: Used to ask the users questions currently just in terminal
-#TODO: add UI and Return to
-def ynQuesntion(q):
-    #speakQuestion(q)
-    answer = input(q)
-    answer.upper()
+#Tony: Now Contains the TTS calls a
+#TODO: add UI code
+def ynQuesntion(q,e):
+
+    b = raw_input("Press any Key to Continue")
+    e.say(q)
+    e.runAndWait()
+    answer = raw_input(q)
+
     if answer == "YES" or answer == "NO" :
         print("ACCEPTED RESPONSE")
         return answer
     else:
         print("INVALID RESPONSE")
-        ynQuesntion(q)
-
-#TonyD: This will use the voice engine to speak the questtion to the user
-#TODO: Create
-def speakQuestion(q):
-    engine.say(q)
-    engine.runAndWait();
+        answer = ynQuesntion(q,e)
+        return answer
 
 
 #TonyD: This is the MAIN secion of the progam all calls through here
-qtext = "does this work?"
+
+#This is The Start TTS
+engine = pyttsx.init()
+engine.say("Questions Now Begin")
+engine.runAndWait()
+
 answers = loadQuestions()
+
 print("Responses were as follows")
 print(answers)
