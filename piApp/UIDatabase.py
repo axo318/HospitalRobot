39229459cc1b63ff15b05498d80610d93721fd78
tonyfduggan <tonyfduggan@gmail.com>
2019-03-12 17:54:35 +0000

    The Database now dynamical sends questions
    The UI has also been updated to match the style of the Web app.

--- piApp/UIDatabase.py
+++ piApp/UIDatabase.py
@@ -19,26 +19,32 @@ class Application(Frame):
         self.MAIN = Label(self)
         self.MAIN["text"] = "PapaDoc"
         self.MAIN["font"] = "Ariel 30"
+        self.MAIN["background"] = "#E8EDEE"
         self.MAIN.grid(row=0, column=0 , columnspan = 2)
 
         self.q = self.patientQs[self.questionNum]
         self.QUESTION = Label(self)
         self.QUESTION["text"] = self.q
         self.QUESTION["font"] = "Ariel 30"
+        self.QUESTION["background"] = "#E8EDEE"
         self.QUESTION.grid(row=1,column=0, columnspan =2)
 
         self.YES = Button(self)
-        self.YES["bg"] = "green"
+        self.YES["bg"] = "#009639"
         self.YES["fg"] = "white"
+        self.YES["activebackground"] = "#009639"
+        self.YES["activeforeground"] = "white"
         self.YES["font"] = "Ariel 30"
-        self.YES["text"] = "YES",
+        self.YES["text"] = "YES"
         self.YES["command"] = self.patientYES
         self.YES.config(height = 5 , width = 15)
         self.YES.grid(row=2,column=0)
 
         self.NO = Button(self)
-        self.NO["bg"] = "red"
+        self.NO["bg"] = "#cc0000"
         self.NO["fg"] = "white"
+        self.NO["activebackground"] = "#cc0000"
+        self.NO["activeforeground"] = "white"
         self.NO["font"] = "Ariel 30"
         self.NO["text"] = "NO"
         self.NO["command"] = self.patientNO
@@ -46,8 +52,10 @@ class Application(Frame):
         self.NO.grid(row = 2, column = 1)
 
         self.btnHelp = Button(self)
-        self.btnHelp["bg"] = "orange"
+        self.btnHelp["bg"] = "#005EB8"
         self.btnHelp["fg"] = "white"
+        self.btnHelp["activebackground"] = "#005EB8"
+        self.btnHelp["activeforeground"] = "white"
         self.btnHelp["font"] = "Ariel 30"
         self.btnHelp["command"] = self.startHelp
         self.btnHelp["text"] = "HELP"
@@ -71,7 +79,7 @@ class Application(Frame):
             self.startFollow()
         self.q = self.patientQs[self.questionNum]
         self.QUESTION["text"] = self.q
-        self.sayQuestion(self.q)
+        #self.sayQuestion(self.q)
 
     def sayQuestion(self,q):
         engine = pyttsx.init()
@@ -90,6 +98,8 @@ class Application(Frame):
         self.widgetConsent()
 
     def startQuestions(self):
+        self.questionNum = 0
+        patientRs = dict()
         self.clearGrid()
         self.patientQs = self.getData()
         self.widgestsQuestions(self.patientQs)
@@ -102,41 +112,54 @@ class Application(Frame):
         self.papaConsent = Label(self)
         self.papaConsent["text"] = "PapaDoc"
         self.papaConsent["font"] = "Ariel 30"
+        self.papaConsent.config(background = "#E8EDEE")
         self.papaConsent.grid(row=0, column=0 , columnspan = 2)
 
         self.lblConsent = Label(self)
-        self.lblConsent["text"] = "Do You consent"
+        self.lblConsent["text"] = "Press Yes for Questions on PapaDoc"
         self.lblConsent["font"] = "Ariel 30"
+        self.lblConsent.config(background = "#E8EDEE")
         self.lblConsent.grid(row=1,column=0, columnspan =2)
 
+        self.lblConsent = Label(self)
+        self.lblConsent["text"] = "Press No to Follow"
+        self.lblConsent["font"] = "Ariel 30"
+        self.lblConsent.config(background = "#E8EDEE")
+        self.lblConsent.grid(row=2,column=0, columnspan =2)
+
         self.btnYConsent = Button(self)
-        self.btnYConsent["bg"] = "green"
+        self.btnYConsent["bg"] = "#009639"
         self.btnYConsent["fg"] = "white"
+        self.btnYConsent["activebackground"] = "#009639"
+        self.btnYConsent["activeforeground"] = "white"
         self.btnYConsent["font"] = "Ariel 30"
         self.btnYConsent["text"] = "YES",
         self.btnYConsent["command"] = self.startQuestions
-        self.btnYConsent.config(height = 5 , width = 15)
-        self.btnYConsent.grid(row=2,column=0)
+        self.btnYConsent.config(height = 3 , width = 15)
+        self.btnYConsent.grid(row=3,column=0)
 
         self.btnNConsent = Button(self)
-        self.btnNConsent["bg"] = "red"
+        self.btnNConsent["bg"] = "#cc0000"
         self.btnNConsent["fg"] = "white"
+        self.btnNConsent["activebackground"] = "#cc0000"
+        self.btnNConsent["activeforeground"] = "white"
         self.btnNConsent["font"] = "Ariel 30"
         self.btnNConsent["text"] = "NO"
         self.btnNConsent["command"] = self.startFollow
-        self.btnNConsent.config(height = 5 , width = 15)
-        self.btnNConsent.grid(row = 2, column = 1)
+        self.btnNConsent.config(height = 3 , width = 15)
+        self.btnNConsent.grid(row = 3, column = 1)
 
         self.btnHelp = Button(self)
-        self.btnHelp["bg"] = "orange"
+        self.btnHelp["bg"] = "#005EB8"
         self.btnHelp["fg"] = "white"
+        self.btnHelp["activebackground"] = "#005EB8"
+        self.btnHelp["activeforeground"] = "white"
         self.btnHelp["font"] = "Ariel 30"
         self.btnHelp["command"] = self.startHelp
         self.btnHelp["text"] = "HELP"
-        self.btnHelp.grid(row = 3, column = 0, columnspan = 2)
+        self.btnHelp.grid(row = 4, column = 0, columnspan = 2)
 
         #self.sayQuestion("Do You consent")
-
 
     def startFollow(self):
         self.clearGrid()
@@ -147,38 +170,51 @@ class Application(Frame):
         self.MAIN = Label(self)
         self.MAIN["text"] = "PapaDoc"
         self.MAIN["font"] = "Ariel 30"
+        self.MAIN["background"] = "#E8EDEE"
         self.MAIN.grid(row=0, column=0 , columnspan = 2)
 
         self.QUESTION = Label(self)
         self.QUESTION["text"] = "Follow Me"
         self.QUESTION["font"] = "Ariel 100"
+        self.QUESTION["background"] = "#E8EDEE"
         self.QUESTION.grid(row=1,column=0, columnspan =2)
 
         self.btnHelp = Button(self)
-        self.btnHelp["bg"] = "orange"
+        self.btnHelp["bg"] = "#005EB8"
         self.btnHelp["fg"] = "white"
+        self.btnHelp["activebackground"] = "#005EB8"
+        self.btnHelp["activeforeground"] = "white"
         self.btnHelp["font"] = "Ariel 30"
         self.btnHelp["command"] = self.finishedTrip
         self.btnHelp["text"] = "At destination"
         self.btnHelp.grid(row = 3, column = 0, columnspan = 2)
 
         self.btnHelp = Button(self)
-        self.btnHelp["bg"] = "orange"
+        self.btnHelp["bg"] = "#005EB8"
         self.btnHelp["fg"] = "white"
+        self.btnHelp["activebackground"] = "#005EB8"
+        self.btnHelp["activeforeground"] = "white"
         self.btnHelp["font"] = "Ariel 30"
         self.btnHelp["command"] = self.startHelp
         self.btnHelp["text"] = "HELP"
-        self.btnHelp.grid(row = 3, column = 0, columnspan = 2)
+        self.btnHelp.grid(row = 4, column = 0, columnspan = 2)
+
+
     def finishedTrip(self):
-        os.remove("piAppStart.txt")
-        open("piAppStart.txt",'a').close
+
+        os.system("echo '' > /var/www/html/piAppStart.txt")
+        #os.system("echo '"+s+"' > /var/www/html/piAppStart.txt")
+        #os.remove("/var/www/html/piAppStart.txt")
+        #open("/var/www/html/piAppStart.txt",'a').close
         self.startWait()
 
     def widgetHelp(self):
 
         self.btnAlert = Button(self)
-        self.btnAlert["bg"] = "orange"
+        self.btnAlert["bg"] = "#005EB8"
         self.btnAlert["fg"] = "white"
+        self.btnAlert["activebackground"] = "#005EB8"
+        self.btnAlert["activeforeground"] = "white"
         self.btnAlert["font"] = "Ariel 30"
         self.btnAlert["text"] = "Alert Staff"
         self.btnAlert["command"] = self.quit
@@ -189,17 +225,20 @@ class Application(Frame):
         self.MAIN = Label(self)
         self.MAIN["text"] = "PapaDoc"
         self.MAIN["font"] = "Ariel 30"
+        self.MAIN.config(background = "#E8EDEE")
         self.MAIN.grid(row=0, column=0 , columnspan = 2)
 
         self.QUESTION = Label(self)
         self.QUESTION["text"] = "Waiting for patient"
         self.QUESTION["font"] = "Ariel 100"
+        self.QUESTION.config(background = "#E8EDEE")
         self.QUESTION.grid(row=1,column=0, columnspan =2)
 
-
         self.btnHelp = Button(self)
-        self.btnHelp["bg"] = "orange"
+        self.btnHelp["bg"] = "#005EB8"
         self.btnHelp["fg"] = "white"
+        self.btnHelp["activebackground"] = "#005EB8"
+        self.btnHelp["activeforeground"] = "white"
         self.btnHelp["font"] = "Ariel 30"
         self.btnHelp["command"] = self.startHelp
         self.btnHelp["text"] = "HELP"
@@ -212,16 +251,20 @@ class Application(Frame):
     def widgetWait(self):
 
         self.btnWait = Button(self)
-        self.btnWait["bg"] = "green"
+        self.btnWait["bg"] = "#009639"
         self.btnWait["fg"] = "white"
+        self.btnWait["activebackground"] = "#009639"
+        self.btnWait["activeforeground"] = "white"
         self.btnWait["font"] = "Ariel 30"
         self.btnWait["command"] = self.check
         self.btnWait["text"] = "Ready To Start"
         self.btnWait.grid(row = 2, column = 0, columnspan = 2)
 
         self.btnHelp = Button(self)
-        self.btnHelp["bg"] = "orange"
+        self.btnHelp["bg"] = "#005EB8"
         self.btnHelp["fg"] = "white"
+        self.btnHelp["activebackground"] = "#005EB8"
+        self.btnHelp["activeforeground"] = "white"
         self.btnHelp["font"] = "Ariel 30"
         self.btnHelp["command"] = self.startHelp
         self.btnHelp["text"] = "HELP"
@@ -229,9 +272,9 @@ class Application(Frame):
 
 
     def check(self):
-        check = os.stat("piAppStart.txt").st_size == 0
+        check = os.stat("/var/www/html/piAppStart.txt").st_size < 2
         while(check):
-            check = os.stat("piAppStart.txt").st_size == 0
+            check = os.stat("/var/www/html/piAppStart.txt").st_size <2
         if(check == False):
             self.startConsent()
 
@@ -239,9 +282,13 @@ class Application(Frame):
 
         db = MySQLdb.connect("localhost","root","password","sdp17" )
         cursor = db.cursor()
-        department = "Cardiology"
-        self.patient = "Tony"
-        self.appdate = "2019-10-10"
+        filename = "/var/www/html/piAppStart.txt"
+        with open(filename) as f:
+            mylist = f.read().splitlines()
+
+        department = mylist[0]
+        self.patient = mylist[1]
+        self.appdate = mylist[2]
         # execute SQL query using execute() method.
         cursor.execute("SELECT Question from Questions WHERE Department = '%s'" % (department))
 
@@ -271,8 +318,6 @@ class Application(Frame):
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.questionNum = 0
-        print(self.questionNum)
         master.title("PapaDoc")
         self.pack()
         self.startWait()
@@ -281,6 +326,10 @@ patientRs = dict()
 
 root = Tk()
 root.attributes('-fullscreen',True)
+root.config(background = "#E8EDEE")
+root.config(cursor="none")
 app = Application(master=root)
+app.config(background = "#E8EDEE")
+app.config(cursor="none")
 app.mainloop()
 root.destroy()
